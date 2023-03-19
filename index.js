f6f217aea9a85f356f91198f9677363c16bbb956
Clifton <clifga@gmail.com>
2023-03-19 13:32:28 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 1;
+let num2 = 62;
+let num3 = 20;
+let num4 = 8;
+const array = [num1, num3, num4]
+const multiply = num1 * num2;
+
+const random = Math.floor((Math.random() + 1))
+
+const mod = num3 % num4
+
+const max = Math.max(...array)
